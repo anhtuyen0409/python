5f8af08b06f72766c058cfbf67f9509d92daeb24
anhtuyen0409 <nguyenanhtuyen10a5@gmail.com>
2026-08-15 22:53:27 +0700

    update strings

--- strings/run.py
+++ strings/run.py
@@ -15,3 +15,21 @@ print(my_string[-1]) # d
 print(my_string[1:4]) # indices: 1, 2, 3 -> kq: 'ell'
 print(my_string[1:]) # indices: 1, 2, 3,... -> kq: 'ello world'
 print(my_string[:-1]) # kq: 'Hello worl'
+
+# noi chuoi
+str_1 = "Hello"
+str_2 = " everyone"
+str_concat = str_1 + str_2
+print(str_concat)
+
+# duyet qua cac ki tu trong chuoi
+my_str = "Hello"
+for str in my_str:
+    print(str)
+
+# kiem tra chuoi con co trong chuoi khong?
+str_3 = "Hello anh em"
+if "anh" in str_3:
+    print("OK")
+else:
+    print("No")
